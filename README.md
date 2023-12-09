27a96fdc9028b770b829c10aba37cb48705042f3
LasseGerstenberg <lasse.gerstenberg@icloud.com>
2023-12-09 08:39:55 +0100

    Initialer Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Verteilte Systeme
  Aufgabe Verteilte Systeme - Synchron vs Asynchron
+
+Lasse Gerstenberg & Nik Kazemifard
+
+--------------------------------------
+Nebenläufigkeit synchron vs. asynchron
+--------------------------------------
+Implementieren Sie jeweils ein Programm mit 2 nebenläufigen Ausführungssträngen für
+- Nutzerein- und ausgaben
+- Countdown
+  in den zwei Varianten
+- asynchrone Kommunikation zwischen der Nutzerein-/ausgabe und Countdown
+- synchrone Kommunikation zwischen der Nutzerein-/ausgabe und Countdown
+
+ Der Countdown soll primär sekündlich herunterzählen, bietet darüber hinaus aber eine Schnittstelle an zum
+- Setzen des Zählers
+- Abbrechen des Countdowns
+- Abfrage des aktuellen Countdownzählers
+- Meldung des Countdownendes an die Nutzerein- und ausgabe
+
+Arbeiten Sie anschließend die fundamentalen Unterschieden zwischen den Lösungen bzgl.
+- Modellierung
+- Implementation
+
+heraus.
---
+++ Verteilte_Systeme/src/Countdown.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Countdown extends Thread {
+
+    private static boolean isCountdownRunning = true;
+    private static int countdownTimeInSeconds = 20;
+
+    public static void main(String[] args) {
+
+        // Userinput
+        Thread userInputThread = new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (isCountdownRunning) {
+                System.out.println("Bitte Befehl eingeben: set {Zahl}, cancel, status");
+                String input = scanner.nextLine();
+
+                if (input.startsWith("set")) {
+                    int value = Integer.parseInt(input.split(" ")[1]);
+                    setCountdown(value);
+                    System.out.println("Countdown wurde auf " + value + " Sekunden gesetzt.");
+                } else if (input.equals("cancel")) {
+                    cancelCountdown();
+                    System.out.println("Countdown wurde abgebrochen.");
+                } else if (input.equals("status")) {
+                    System.out.println("Countdown in Sekunden: " + getCountdown());
+                }
+            }
+        });
+
+        // Countdown
+        Thread countdownThread = new Thread(() -> {
+            while (isCountdownRunning) {
+                try {
+                    Thread.sleep(1000); // 1000 = 1 Sekunde
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (isCountdownRunning) {
+                    countdownTimeInSeconds--;
+                    if (countdownTimeInSeconds <= 0) {
+                        isCountdownRunning = false;
+                        System.out.println("Countdown abgelaufen!");
+                    }
+                }
+            }
+        });
+
+        userInputThread.start();
+        countdownThread.start();
+
+        try {
+            userInputThread.join();
+            countdownThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void setCountdown(int value) {
+        countdownTimeInSeconds = value;
+    }
+
+    private static void cancelCountdown() {
+        isCountdownRunning = false;
+    }
+
+    private static int getCountdown() {
+        return countdownTimeInSeconds;
+    }
+}
